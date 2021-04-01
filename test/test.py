3264d40a54342401103e8d9ab91d286a39fef29a
Ian Mercadal <imercadal@cifpfbmoll.eu>
2021-04-01 17:07:11 +0200

    test: Adding easy tests
    Adding two easy tests to see if the code is enough good

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
-from src.main import Bolera
+from src.main import Bowls
 
 def test_bolera():
-    assert 60 == Bolera("12345123451234512345").calcular_puntos()
+    assert 30 == Bowls("X111111111111111111").calcular_puntos()
+    assert 61 == Bowls("12345123451234512346").calcular_puntos()
